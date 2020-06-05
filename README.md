55f051eae8c1e00985816d48ca1080b7cf30549d
alexishlg <alexiparra@gmail.com>
2020-06-05 17:08:33 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+#Ejemplo agregando Search in RecyclerView usando Kotlin
+![logo](https://i.imgur.com/8mkJZoI.jpg)
+> Hospital Lucia 2020
+
+![](Screenshot_1.jpg)
+![](Screenshot_2.jpg)
+![](Screenshot_3.jpg)
+
+Alexis Parra– [@cubanito_alex](https://twitter.com/cubanito_alex) – alexiparra@gmail.com
---
+++ app/src/main/java/cu/sld/hluciasearchrecyclerview/DetailsActivity.kt
@@ -0,0 +1,18 @@
+package cu.sld.hluciasearchrecyclerview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sld.hluciasearchrecyclerview.R
+import kotlinx.android.synthetic.main.activity_details.*
+
+class DetailsActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+        detail_country_text.text = intent.extras!!.getString("passselectedcountry")!!
+
+    }
+}
